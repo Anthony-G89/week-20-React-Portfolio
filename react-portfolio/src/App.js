8fc4d14a51c89c137d04306d57f7491b7a8bb810
Anthony Guerrero <adguerrero1935@gmail.com>
2020-05-21 15:04:38 -0400

    Trying to get header to work

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../src/components/Header";
+// import Footer from "../src/components/Footer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Header />
+            </div>
+
+
+
+
+        </Router>
+
+
+
+    )
 }
 
-export default App;
+export default App; 
